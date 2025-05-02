ec0e1b30ef78c89626c3b1f4bd76248ee2df0c24
wiembenrhouma <wiem.benrhouma@esprit.tn>
2025-05-02 23:20:02 +0200

    Update CORS configuration to allow frontend from Vercel

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const path = require("path");
 
 app.use(express.json());
 app.use(cors({
-    origin: 'https://e-commerce-app-4upc.vercel.app/'  
+    origin: 'https://e-commerce-app-4upc.vercel.app'  
   }));
 
 
