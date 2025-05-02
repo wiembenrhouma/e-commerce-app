4ec01151bad9ef97d93ccc59189acb966535c2eb
wiembenrhouma <wiem.benrhouma@esprit.tn>
2025-05-03 00:48:51 +0200

    Fix CORS configuration for Vercel frontend4

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -27,7 +27,7 @@ const AddProduct = () =>{
         let formData = new FormData();
         formData.append('product',image);
 
-        await fetch('http://localhost:4000/upload',{
+        await fetch('https://e-commerce-app-p6bd.onrender.com/upload',{
           method:'POST',
           headers:{
             Accept:'application/json',
@@ -39,7 +39,7 @@ const AddProduct = () =>{
         {
           product.image = responseData.image_url;
           console.log(product);
-          await fetch('http://localhost:4000/addproduct',{
+          await fetch('https://e-commerce-app-p6bd.onrender.com/addproduct',{
             method:'POST',
             headers:{
               Accept:'application/json',
